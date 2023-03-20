aa26f0d98aa249846f458da13d7b81c9144fcb3d
elsonchan <elsonccj@gmail.com>
2023-03-21 00:22:37 +0800

    Updated repository file

--- src/main/java/ElsonTFIP/day21/OwnPractice/model/TVShow.java
+++ src/main/java/ElsonTFIP/day21/OwnPractice/model/TVShow.java
@@ -1,59 +1,20 @@
 package ElsonTFIP.day21.OwnPractice.model;
 
-import org.springframework.jdbc.support.rowset.SqlRowSet;
+//import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TVShow {
     private Integer id;
-    private String name;
-    private String network;
-    private Integer rating;
+    private String title;
+    private String genre;
+    private Float rating;
 
-    public TVShow() {
-        // Default constructor
-    }
-
-    public TVShow(Integer id, String name, String network, Integer rating) {
-        this.id = id;
-        this.name = name;
-        this.network = network;
-        this.rating = rating;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNetwork() {
-        return network;
-    }
-
-    public void setNetwork(String network) {
-        this.network = network;
-    }
-
-    public Integer getRating() {
-        return rating;
-    }
-
-    public void setRating(Integer rating) {
-        this.rating = rating;
-    }
 
     // public static TVShow populate(SqlRowSet rs) {
     //     TVShow tvShow = new TVShow();
--- src/main/java/ElsonTFIP/day21/OwnPractice/repository/TVShowRepository.java
+++ src/main/java/ElsonTFIP/day21/OwnPractice/repository/TVShowRepository.java
@@ -1,18 +1,41 @@
 package ElsonTFIP.day21.OwnPractice.repository;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 
+import ElsonTFIP.day21.OwnPractice.model.TVShow;
+
 public class TVShowRepository {
 
+    //JdbcTemplate acts as a connection pool between DB(mysql) and spring application
+    //This connection pool is configured in the application properties folder with the DB url, username, password. 
     @Autowired
     private JdbcTemplate template;
 
-    public SqlRowSet get(final Integer tvId) {
-        String sql = "SELECT * FROM TVShows WHERE id = ?";
-        return template.queryForRowSet(sql, tvId);
+    public List<TVShow> get(final int limit, final int offset) {
+
+        final List<TVShow> result = new LinkedList<>();
+
+        final SqlRowSet rs = template.queryForRowSet(
+            "select * from TVShows limit ? offset ?", 3, 0);
+    
+
+        while (rs.next()) {
+            TVShow tv = new TVShow();
+            tv.setId(rs.getInt("id"));
+            tv.setTitle(rs.getString("title"));
+            tv.setGenre(rs.getString("genre"));
+            tv.setRating(rs.getFloat("rating"));
+            result.add(tv);
+
+        }
         
+        return (Collections.unmodifiableList(result));
     }
     
 }
